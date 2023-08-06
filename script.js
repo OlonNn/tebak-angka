fd08a1de8b199c8488f78b58fb70e726362821cf
Olonn <fitrah281103@gmail.com>
2023-08-06 18:51:42 +0800

    firts commit, add html and Js for games

---
+++ script.js
@@ -0,0 +1,37 @@
+// Game Tebak angka
+var tanya = true;
+while (tanya) {
+  var chance = 3;
+  while (chance > 0) {
+    var p = parseInt(
+      prompt(
+        "Masukkan angka tebakanmu!! \nBilangan bulat 1-10 \nKamu punya " +
+          chance +
+          "x kesempatan"
+      )
+    );
+    console.log(p);
+
+    var y = 10;
+    var bot = Math.floor(Math.random() * y) + 1;
+
+    hasil = "";
+    if (p == bot) {
+      alert("Selamat!! tebakan anda benar " + "\nAngka yang dicari " + bot);
+      break;
+    } else if (p < bot && p <= 10 && p > 0) {
+      alert("Angka terlalu rendah!!");
+    } else if (p > bot && p <= 10 && p > 0) {
+      alert("Angka terlalu besar!!");
+    } else {
+      hasil = "Hasil tidak sesui dengan kriteria";
+    }
+    console.log(hasil)
+    alert('Tebakanmu ' + p);
+    chance--;
+  }
+
+  tanya = confirm("Lagi?");
+}
+
+alert("TERIMA KASIH TELAH BERMAIN");
